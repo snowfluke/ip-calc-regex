5d9f3fcbfb59705d7fd782ad3bd2ceae2f228055
snowfluke <awalariansyah7@gmail.com>
2021-05-30 20:36:46 +0700

    fix host max, total subnet, and outrange cidr checking

--- index.js
+++ index.js
@@ -23,12 +23,28 @@ const ipCalc = (ip, cat, val) => {
 	}
   
   if(cat == 1 && !/\b([1-9]|[12][0-9]|3[0-2])\b/.test(val)) return {errorMsg: "Invalid CIDR!"}
-  
   if(cat == 2 && !/^[1-9]\d*$/.test(val)) return {errorMsg: "Invalid Host number!"}
 
+  const classify = ip => {
+    if (ip < 128) return "Class A"
+    if (ip < 192) return "Class B"
+    if (ip < 224) return "Class C"
+    if (ip < 240) return "Class D"
+    if (ip < 256) return "Class E"
+  }
+
+  const ipArr = result["IP Address"].split('.')
+  result.Class = classify(ipArr[0])
+  
   const MAX_BIT = 32
   const MAX_BIN = 255
   const BITS_32 = 4294967295
+
+  const classifyCIDR = cidr => {
+    if (cidr < 16) return 0
+    if (cidr < 24) return 1
+    if (cidr < 32) return 2
+  }
 
   const unpackInt = i => -1 << (MAX_BIT - i);
   const intToQdot = i => [
@@ -55,14 +71,6 @@ const ipCalc = (ip, cat, val) => {
   
     return x;
   }
-
-  const classify = ip => {
-    if (ip < 128) return "Class A"
-    if (ip < 192) return "Class B"
-    if (ip < 224) return "Class C"
-    if (ip < 240) return "Class D"
-    if (ip < 256) return "Class E"
-}
 
   const getNetworkAddress = (ipBit, netmaskBit) => intToQdot(ipBit & netmaskBit);
   const getBroadcastAddress = (ipBit, netmaskBit) => intToQdot(ipBit | (~netmaskBit & BITS_32));
@@ -92,14 +100,19 @@ const ipCalc = (ip, cat, val) => {
     result["Total Hosts"] = usableHost(hostPerSubnet(result.CIDR))
   }
 
-  const ipArr = result["IP Address"].split('.')
+  let CIDRclass = classifyCIDR(result.CIDR)
+  const outRangeMsg = { errorMsg: "IP Address and Netmask/CIDR out of range!"}
+
+  if(result.Class === "Class A" && CIDRclass != 0) return outRangeMsg
+  if(result.Class === "Class B" && CIDRclass != 1) return outRangeMsg
+  if(result.Class === "Class C" && CIDRclass != 2) return outRangeMsg
+
   const netmaskArr = result.Netmask.split('.')
 
   const ipBit = qdotToInt(ipArr)
   const netmaskBit = qdotToInt(netmaskArr)
 
   result.Wildcard = intToQdot(~netmaskBit)
-  result.Class = classify(ipArr[0])
 
   result["Network Address"] = getNetworkAddress(ipBit, netmaskBit)
   result.Broadcast = getBroadcastAddress(ipBit, netmaskBit)
@@ -113,8 +126,35 @@ const ipCalc = (ip, cat, val) => {
   result["Host min"] = networkInc.join('.')
   result["Host max"] = broadcastDec.join('.')
 
-  result["Total Subnets"] = (result.CIDR % 8) ? 
-    Math.pow(2, (result.CIDR % 8)) : Math.pow(2, 8)
+  const subnetBlockTotal = () => {
+    if(result.Class === "Class A"){
+      let pow =
+      parseInt(netmaskArr[1], 10).toString(2) +
+      parseInt(netmaskArr[2], 10).toString(2) +
+      parseInt(netmaskArr[3], 10).toString(2)
+      pow = pow.split('').filter( el => el != 0).length
+      return Math.pow(2, pow)
+    }
+
+    if(result.Class === "Class B"){
+      let pow =
+      parseInt(netmaskArr[1], 10).toString(2) +
+      parseInt(netmaskArr[2], 10).toString(2) 
+      pow = pow.split('').filter( el => el != 0).length
+      return Math.pow(2, pow)
+    }
+
+    if(result.Class === "Class C"){
+      let pow =
+      parseInt(netmaskArr[3], 10).toString(2)
+      pow = pow.split('').filter( el => el != 0).length
+      return Math.pow(2, pow)
+    }
+
+    return 0
+  }
+
+  result["Total Subnets"] = subnetBlockTotal()
     
   result.CIDR = '/'+result.CIDR
   
